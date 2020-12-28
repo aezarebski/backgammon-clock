f6f5a08a3dded8418bf1c394b4516b9f84a9f797
aezarebski <aezarebski@gmail.com>
2020-12-28 11:04:27 +0000

    fix a couple of errors that tsc found

--- bgclock.js
+++ bgclock.js
@@ -1,16 +1,14 @@
 // This is used as a way to specify which key starts the timer for each player.
 var PlayerKey = {
-    PLAYERONE: 70,              // "f" key
-    PLAYERTWO: 74               // "j" key
+    PLAYERONE: 70,
+    PLAYERTWO: 74 // "j" key
 };
-
 // This is used to specify which player's clock is currently running.
 var ClockState = {
     OFF: 0,
     PLAYERONE: 1,
     PLAYERTWO: 2
 };
-
 // The number of minutes allocated to each player at the start of the match.
 var numMinutes = 3;
 // The number of seconds that each player gets to make a move.
@@ -18,43 +16,37 @@ var numSecondsPerPlay = 15;
 var playerOneTime = numMinutes * 60;
 var playerTwoTime = numMinutes * 60;
 var gameClockState = ClockState.OFF;
-
-
-document.getElementById("playerOneTime").innerHTML = playerOneTime;
-document.getElementById("playerTwoTime").innerHTML = playerTwoTime;
-
-
+document.getElementById("playerOneTime").innerHTML = playerOneTime.toString();
+document.getElementById("playerTwoTime").innerHTML = playerTwoTime.toString();
 function runTimerOne() {
     playerOneTime--;
-    document.getElementById("playerOneTime").innerHTML = playerOneTime;
+    document.getElementById("playerOneTime").innerHTML = playerOneTime.toString();
     if (playerOneTime > 0) {
         if (gameClockState == ClockState.PLAYERONE) {
             var t = setTimeout(runTimerOne, 1000);
         }
-    } else {
+    }
+    else {
         clockRanOut(1);
     }
 }
-
 function runTimerTwo() {
     playerTwoTime--;
-    document.getElementById("playerTwoTime").innerHTML = playerTwoTime;
+    document.getElementById("playerTwoTime").innerHTML = playerTwoTime.toString();
     if (playerTwoTime > 0) {
         if (gameClockState == ClockState.PLAYERTWO) {
             var t = setTimeout(runTimerTwo, 1000);
         }
-    } else {
+    }
+    else {
         clockRanOut(2);
     }
 }
-
-
 function clockRanOut(playerNumber) {
     gameClockState = ClockState.OFF;
     var message = (playerNumber == 1 ? "Red" : "White") + " player ran out of time!";
     document.getElementById("result").innerHTML = message;
 }
-
 function startOne() {
     document.getElementById("playerOneName").className = "playerOneOn";
     document.getElementById("playerOneTime").className = "playerOneOn";
@@ -65,11 +57,10 @@ function startOne() {
     if (gameClockState != ClockState.PLAYERONE) {
         gameClockState = ClockState.PLAYERONE;
         playerOneTime += numSecondsPerPlay;
-     	  console.log("starting player one timer");
+        console.log("starting player one timer");
         runTimerOne();
     }
 }
-
 function startTwo() {
     document.getElementById("playerOneName").className = "playerOneOff";
     document.getElementById("playerOneTime").className = "playerOneOff";
@@ -84,16 +75,17 @@ function startTwo() {
         runTimerTwo();
     }
 }
-
 function respondToEvent() {
     if (event.which == PlayerKey.PLAYERONE) {
         startOne();
-    } else if (event.which == PlayerKey.PLAYERTWO) {
+    }
+    else if (event.which == PlayerKey.PLAYERTWO) {
         startTwo();
-    } else {
+    }
+    else {
         console.log("neither f nor j so not doing anything...");
         console.log("the event code is " + event.which);
     }
-};
-
+}
+;
 $('body').on('keyup', respondToEvent);
